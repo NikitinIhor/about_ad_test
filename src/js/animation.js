86170db954270cdf3a2aa309d5a504532cfc1f36
Nikitin <nikitinigor0907@gmail.com>
2025-06-19 15:29:14 +0200

    feat/section 2

---
+++ src/js/animation.js
@@ -0,0 +1,19 @@
+export function animation() {
+  const animatedElements = document.querySelectorAll(".animation");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  animatedElements.forEach((el) => observer.observe(el));
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import "modern-normalize/modern-normalize.css";
 import "./styles/main.scss";
+
+import { animation } from "./js/animation.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  animation();
+});
